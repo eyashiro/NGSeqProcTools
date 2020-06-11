3d1f18a4d051a9158136a0480dcc65fd7dec5370
Erika Y <eyashiro@users.noreply.github.com>
2020-06-11 13:32:03 +0200

    Config file to source, includes path to sequences.

---
+++ NGSeqProcToolsConfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#############
+# PARAMETERS
+#############
+
+# Path to Illumina sequences
+SEQPATH="/space/sequences/"
+
+######################
+# getseqID parameters
+######################
+
+# Reference seqID/libID list location and file name
+#REFPATH=/home/erika/Documents/test_LIB/tmpout3
+REFPATH=/space/HiSeqUser/SeqLibID_reference
+REFLIST=SeqLibID.txt
