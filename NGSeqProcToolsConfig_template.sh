414d187ea435f3841595d1856f03dfac3947d04b
Erika Y <eyashiro@users.noreply.github.com>
2020-06-11 13:44:41 +0200

    template config file to fill out prior to running NGSeqProcTools

---
+++ NGSeqProcToolsConfig_template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#############
+# PARAMETERS
+#############
+
+# Path to Illumina sequences
+SEQPATH=
+
+######################
+# getseqID parameters
+######################
+
+# Reference seqID/libID list location and file name
+#REFPATH=/home/erika/Documents/test_LIB/tmpout3
+REFPATH=
+REFLIST=SeqLibID.txt
